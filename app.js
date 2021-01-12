a71517a00496ac2a08564d506335c901abf36567
taylorreichner <taylorreichner@yahoo.com>
2021-01-12 15:52:32 -0800

    added functionality to the clicker button

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
 // import functions and grab DOM elements
+const numberGuessed = document.getElementById('input-box');
+const submitButton = document.getElementById('submit-button');
+const secretGuessesLeft = document.getElementById('secret-guesses-left');
+const wrongAnswer = document.getElementById('wrong-answer');
+const endResult = document.getElementById('end-result');
+const randomNumber = Math.ceil(Math.random() * 20);
+let guessesLeft = 4;
+submitButton.addEventListener('click', () => {
+    const winString = 'You guessed the number!';
+    const tooLow = 'Your guess was too low!';
+    const tooHigh = 'Your guess was too high!';
+    
+    
+    guessesLeft--;
 
-// initialize state
 
-// set event listeners to update state and DOM
+     if (numberGuessed.value == randomNumber) {
+       endResult.textContent = winString; }
+       
+    if (numberGuessed.value < randomNumber) {
+        endResult.textContent = tooLow }
+    
+    if (numberGuessed.value > randomNumber) {
+        endResult.textContent = tooHigh }
+
+    
+      
+      
+
+        
+    
+        const numberOfGuesses = `You have ${guessesLeft} guesses Left!`;
+
+    if (numberGuessed.value) {
+        secretGuessesLeft.textContent = numberOfGuesses }
+
+       const youLose = 'You failed to guess the correct number';
+        if (guessesLeft == 0) {
+            endResult.textContent = youLose } 
+
+         
+    
+   
+
+});
