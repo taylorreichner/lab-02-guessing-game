1afbdc9369fe0df0bb74eb75f2dd54f97df59b2f
taylorreichner <taylorreichner@yahoo.com>
2021-01-12 16:07:02 -0800

    fixed bug where you lost if you got the correct anser on your last turn

--- app.js
+++ app.js
@@ -6,17 +6,18 @@ const wrongAnswer = document.getElementById('wrong-answer');
 const endResult = document.getElementById('end-result');
 const randomNumber = Math.ceil(Math.random() * 20);
 let guessesLeft = 4;
+const resetButton = document.getElementById('reset-button');
+
+
 submitButton.addEventListener('click', () => {
     const winString = 'You guessed the number!';
     const tooLow = 'Your guess was too low!';
     const tooHigh = 'Your guess was too high!';
     
-    
     guessesLeft--;
 
-
-     if (numberGuessed.value == randomNumber) {
-       endResult.textContent = winString; }
+    if (numberGuessed.value == randomNumber) {
+       endResult.textContent = winString }
        
     if (numberGuessed.value < randomNumber) {
         endResult.textContent = tooLow }
@@ -24,23 +25,17 @@ submitButton.addEventListener('click', () => {
     if (numberGuessed.value > randomNumber) {
         endResult.textContent = tooHigh }
 
-    
-      
-      
-
-        
-    
         const numberOfGuesses = `You have ${guessesLeft} guesses Left!`;
 
     if (numberGuessed.value) {
         secretGuessesLeft.textContent = numberOfGuesses }
 
        const youLose = 'You failed to guess the correct number';
-        if (guessesLeft == 0) {
+        
+    if (guessesLeft == 0) {
             endResult.textContent = youLose } 
 
-         
+    if (guessesLeft == 0, numberGuessed.value == randomNumber) {
+        endResult.textContent = winString }
     
-   
-
 });
